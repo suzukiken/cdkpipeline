e05b8a8f0dc1439beec88b83d77938b8d6f5ef0e
suzukiken <suzukiken@gmail.com>
2021-05-16 23:39:52 +0000

    python to ts

--- lib/cdkpipeline-stack.ts
+++ lib/cdkpipeline-stack.ts
@@ -1,7 +1,8 @@
 import * as apigw from '@aws-cdk/aws-apigateway';
 import * as lambda from '@aws-cdk/aws-lambda';
-import { PythonFunction } from '@aws-cdk/aws-lambda-python';
+// import { PythonFunction } from '@aws-cdk/aws-lambda-python';
 import * as cdk from '@aws-cdk/core';
+import * as path from 'path';
 
 export class CdkpipelineStack extends cdk.Stack {
 
@@ -10,11 +11,19 @@ export class CdkpipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
+    /*
     const handler = new PythonFunction(this, "Handler", {
       entry: "lambda",
       index: "main.py",
       handler: "lambda_handler",
       runtime: lambda.Runtime.PYTHON_3_8
+    })
+    */
+    
+    const handler = new lambda.Function(this, 'Lambda', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'handler.handler',
+      code: lambda.Code.fromAsset(path.resolve(__dirname, 'lambda')),
     })
     
     const gw = new apigw.LambdaRestApi(this, 'Gateway', {
